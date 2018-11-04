83dd32ff3cbe571793c36e022eb9c6186af377bc
Quimberton <44734859+Quimberton@users.noreply.github.com>
2018-11-04 10:35:56 -0500

    Add files via upload

---
+++ UniversalCLient.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class UniversalCLient {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String key = scan.nextLine().toLowerCase();
+		Game game;
+		switch(key) {
+		
+		case "anagrams":
+			game = new Anagram(6);
+			break;
+			
+		case "mastermind":
+			game = new Mastermind(6);
+			break;
+		
+		case "hangman":
+			game = new HangMan(6);
+			break;
+			
+		case "sudoku":
+			game = new GameBoard();
+			break;
+		}
+	}
+
+}
